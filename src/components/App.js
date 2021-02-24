f4b4023ef0d5edc288e9ef11f28074ecea344cae
jechfernandez <jechfernandez@gmail.com>
2021-02-24 09:11:51 +0100

    Run docs test

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokeList from './PokeList';
 import pokemons from '../data/data.json';
+
+function App() {
+    const [pokemonData, setPokemonData] = useState(pokemons);
+
+    return
+} 
+
+export default App;
